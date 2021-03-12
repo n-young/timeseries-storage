2d0c5987fda7a966075f3b4e790f63c3bc190c2d
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2021-03-11 23:36:21 -0500

    Replace mutex over index with a reader-writer lock
    A reader-writer lock allows us to hold multiple read locks on the
    index, which is the usual use case when inserting data into existing
    time series, or when reading data from existing time series. Write
    locks on the index are only held when a completely new series is being
    added. This solves the performance issue of the entire index being
    locked whenever a read/write occurs.

--- src/server/store.rs
+++ src/server/store.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashMap,
-    sync::{mpsc::Receiver, Arc, Mutex},
+    sync::{mpsc::Receiver, Arc, Mutex, RwLock},
     thread,
 };
 
@@ -23,7 +23,7 @@ impl Series {
     }
 }
 
-fn db_read(read_rx: Receiver<SelectRequest>, index: Arc<Mutex<HashMap<String, Series>>>) {
+fn db_read(read_rx: Receiver<SelectRequest>, index: Arc<RwLock<HashMap<String, Series>>>) {
     // Receive read operations from the server
     for request in read_rx {
         let statement = request.statement.clone();
@@ -32,27 +32,30 @@ fn db_read(read_rx: Receiver<SelectRequest>, index: Arc<Mutex<HashMap<String, Se
     }
 }
 
-fn db_write(write_rx: Receiver<Record>, index: Arc<Mutex<HashMap<String, Series>>>) {
+fn db_write(write_rx: Receiver<Record>, storage: Arc<RwLock<HashMap<String, Series>>>) {
     // Receive write operations from the server
     for received in write_rx {
         let key: String = received.get_key();
-        let mut db = index.lock().unwrap();
-        match db.get_mut(&key) {
-            Some(v) => {
-                println!("Received a familiar key!");
-                v.insert(received);
-            }
-            None => {
-                println!("First time seeing this key.");
-                db.insert(key, Series::new(received));
-            }
+        // Obtain a read lock on storage
+        let map = storage.read().expect("RwLock poisoned");
+        //
+        if let Some(series) = map.get(&key) {
+            println!("Received a familiar key!");
+            series.insert(received);
+            continue;
         }
+        // Key does not exist in map
+        println!("First time seeing this key.");
+        // Replace read lock with a write lock
+        drop(map);
+        let mut map = storage.write().expect("RwLock poisoned");
+        map.insert(key, Series::new(received));
     }
 }
 
 pub fn db_open(read_rx: Receiver<SelectRequest>, write_rx: Receiver<Record>) {
     // Create an in-memory storage structure
-    let index = Arc::new(Mutex::new(HashMap::new()));
+    let index = Arc::new(RwLock::new(HashMap::new()));
 
     // Set up separate r/w threads so that read operations don't block writes
     let read_index = Arc::clone(&index);
