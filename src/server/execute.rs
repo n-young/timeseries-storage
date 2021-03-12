97c327b5359a272a5022957feb46e8524bf608e5
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2021-03-11 23:10:36 -0500

    Add a separate thread to handle read requests
    Read requests are now sent through a SelectRequest struct that couples
    the select statement together with a channel to return the
    results.
    
    Additionally, the index is now wrapped in a mutex that's itself
    wrapped in a thread-safe reference-counting pointer. This enables the
    index to be read/written in multiple threads. There are performance
    tradeoffs to this because the entire index has to be locked on
    read/write, so one possibility is that we could shard the index into
    multiple hashmaps.

--- src/server/execute.rs
+++ src/server/execute.rs
@@ -1,16 +1,49 @@
-use crate::server::operators::Op;
+use crate::server::operators::{Op, Select};
 use crate::{error::Error, server::record::Record};
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{channel, Receiver, Sender};
 
-pub fn execute(operation: Op, tx: &Sender<Record>) -> Result<Option<Vec<Record>>, Error> {
-    match operation {
-        Op::Write(record) => execute_write(record, tx),
-        Op::Select(_) => {
-            println!("SELECT!");
-            Ok(None)
-        },
-        _ => Ok(None),
+pub struct SelectRequest {
+    pub statement: Select,
+    result_tx: Sender<Vec<Record>>,
+}
+
+impl SelectRequest {
+    fn new(s: Select) -> (Self, Receiver<Vec<Record>>) {
+        let (tx, rx): (Sender<Vec<Record>>, Receiver<Vec<Record>>) = channel();
+        (
+            SelectRequest {
+                statement: s,
+                result_tx: tx,
+            },
+            rx,
+        )
     }
+
+    pub fn reply(&self, r: Vec<Record>) {
+        self.result_tx.send(r).unwrap();
+    }
+}
+
+pub fn execute(
+    operation: Op,
+    read_tx: &Sender<SelectRequest>,
+    write_tx: &Sender<Record>,
+) -> Result<Option<Vec<Record>>, Error> {
+    match operation {
+        Op::Write(record) => execute_write(record, write_tx),
+        Op::Select(statement) => execute_select(statement, read_tx),
+    }
+}
+
+fn execute_select(
+    statement: Select,
+    tx: &Sender<SelectRequest>,
+) -> Result<Option<Vec<Record>>, Error> {
+    let (request, rx) = SelectRequest::new(statement);
+    tx.send(request).unwrap();
+    let result = rx.recv().unwrap();
+    println!("Received result: {:?}", result);
+    Ok(Some(result))
 }
 
 fn execute_write(record: Record, tx: &Sender<Record>) -> Result<Option<Vec<Record>>, Error> {
--- src/server/store.rs
+++ src/server/store.rs
@@ -1,24 +1,64 @@
-use std::collections::HashMap;
-use std::sync::mpsc::Receiver;
+use std::{
+    collections::HashMap,
+    sync::{mpsc::Receiver, Arc, Mutex},
+    thread,
+};
 
-use crate::server::record::Record;
+use crate::server::{execute::SelectRequest, operators::Select, record::Record};
 
-pub fn db_open(write_rx: Receiver<Record>) {
-    // Create an in-memory storage structure
-    let mut index: HashMap<String, Vec<Record>> = HashMap::new();
+struct Series {
+    records: Mutex<Vec<Record>>,
+}
 
+impl Series {
+    fn new(record: Record) -> Self {
+        Series {
+            records: Mutex::new(vec![record]),
+        }
+    }
+
+    fn insert(&self, record: Record) {
+        let mut v = self.records.lock().unwrap();
+        v.push(record);
+    }
+}
+
+fn db_read(read_rx: Receiver<SelectRequest>, index: Arc<Mutex<HashMap<String, Series>>>) {
+    // Receive read operations from the server
+    for request in read_rx {
+        let statement = request.statement.clone();
+        println!("Received statement: {:?}", statement);
+        request.reply(Vec::new());
+    }
+}
+
+fn db_write(write_rx: Receiver<Record>, index: Arc<Mutex<HashMap<String, Series>>>) {
     // Receive write operations from the server
     for received in write_rx {
         let key: String = received.get_key();
-        match index.get_mut(&key) {
+        let mut db = index.lock().unwrap();
+        match db.get_mut(&key) {
             Some(v) => {
                 println!("Received a familiar key!");
-                v.push(received);
+                v.insert(received);
             }
             None => {
                 println!("First time seeing this key.");
-                index.insert(key, vec![received]);
+                db.insert(key, Series::new(received));
             }
         }
     }
+}
+
+pub fn db_open(read_rx: Receiver<SelectRequest>, write_rx: Receiver<Record>) {
+    // Create an in-memory storage structure
+    let index = Arc::new(Mutex::new(HashMap::new()));
+
+    // Set up separate r/w threads so that read operations don't block writes
+    let read_index = Arc::clone(&index);
+    let read_thr = thread::spawn(move || db_read(read_rx, read_index));
+    let write_index = Arc::clone(&index);
+    let write_thr = thread::spawn(move || db_write(write_rx, write_index));
+    read_thr.join().unwrap();
+    write_thr.join().unwrap();
 }
